96b8c8d36735fe58aed12415351e753ce4d31ff8
celiasallet <celiasallet@yahoo.fr>
2025-12-14 17:33:06 +0100

    cards rsvp

--- scriptrsvp.js
+++ scriptrsvp.js
@@ -1,39 +1,5 @@
 const API_URL = 'https://script.google.com/macros/s/AKfycbxY2_7kuMrdNJ7xNKzA1sdDWhfBm_xBBwzEYASmditw-IkzgV5p5E3wPbeAZJDRYh7L/exec';
 
-const form = document.getElementById('trip-form');
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    const tripData = {
-        driver: document.getElementById('driver').value,
-        departure: document.getElementById('departure').value,
-        seats_total: document.getElementById('trip-seats').value
-    };
-
-    fetch(API_URL, {
-        method: 'POST',
-        body: JSON.stringify(tripData)
-    })
-    .then(res => res.json())
-    .then(data => {
-        if (data.success) {
-            alert('Trajet ajouté 🚗');
-            form.reset();
-        } else {
-            alert('Erreur : ' + data.error);
-        }
-    })
-    .catch(err => {
-        alert('Erreur réseau');
-        console.error(err);
-    });
-});
-
-
-///////////////////////////////////////
-
-
 function renderTrips(trips) {
   const container = document.getElementById('trips-container');
   container.innerHTML = ''; // reset container
@@ -66,3 +32,4 @@ fetch(API_URL)
   .then(res => res.json())
   .then(data => renderTrips(data))
   .catch(err => console.error('Erreur récupération trajets', err));
+
