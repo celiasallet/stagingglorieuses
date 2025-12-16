58307039dda1e3e25c11b4fa95152b8f99e605f3
celiasallet <celiasallet@yahoo.fr>
2025-12-16 19:03:22 +0100

    subit ?

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('rsvp-form');
-    const popup = document.getElementById('thankyou-popup');
+// script de formulaire pour pop window
+const form = document.getElementById('rsvp-form');
+const popup = document.getElementById('thankyou-popup');
+const closeBtn = popup.querySelector('.close-btn');
 
-    if (!form || !popup) return;
-
-    const closeBtn = popup.querySelector('.close-btn');
-
-    form.addEventListener('submit', function(e) {
-        e.preventDefault();
-
+    form.addEventListener('submit', function() {
         popup.style.display = "flex";
 
         const btn = form.querySelector('button[type="submit"]');
         btn.textContent = "C'est noté !";
         btn.disabled = true;
-
-        form.submit();
     });
 
-    closeBtn.addEventListener('click', () => {
+    closeBtn.addEventListener('click', function() {
         popup.style.display = "none";
     });
 
-    popup.addEventListener('click', (e) => {
-        if (e.target === popup) popup.style.display = "none";
+    popup.addEventListener('click', function(e) {
+        if (e.target === popup) {
+        popup.style.display = "none";
+        }
     });
-});
